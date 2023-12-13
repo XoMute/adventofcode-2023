fe9f0dc3bf209d3c445e03419180c82c4986ba93
Dmytro Khomutnyk <dmytrokh@wix.com>
2023-12-13 21:53:34 +0200

    Day 10 solved

--- day10/b.clj
+++ day10/b.clj
@@ -66,36 +66,93 @@
 
 ;; Returns count of straight pipes in chain from starting position
 (defn count-straight-pipes-in-chain [pipes chain [y x]]
-  (if (seq (filter #(= (second %) [y x]) chain)) ;; TODO: remove if?
-    [0 0 0 0]
-    (let [up (range y)
+  (if (seq (filter #(= (second %) [y x]) chain)) ;; Skip the chain itself TODO: remove if?
+    0 #_[0 0 0 0]
+    (let [horizontal-wall? #{\- \F \J \L \7}
+          vertical-wall? #{\| \F \J \L \7}
+          up (range y)
           down (range (+ y 1) (count pipes))
           left (range x)
-          right (range (+ x 1) (count pipes))]
-      [(->> up
-            (map (fn [new-y]
-                   (first (filter (fn [[pipe [ny nx]]] (and (= pipe \-) 
-                                                            (= [ny nx] [new-y x]))) chain))))
-            (remove nil?)
-            count)
+          right (range (+ x 1) (count (first pipes)))]
+
+      #_[(->> up
+            (reduce 
+             (fn [[prev-angle count] new-y]
+               (let [[pipe [ny nx]]
+                     (first (filter 
+                             (fn [[pipe [ny nx]]] (and (horizontal-wall? pipe) 
+                                                       (= [ny nx] [new-y x]))) chain))]
+                 (cond
+                   (and (= prev-angle \F) (= pipe \J)) [nil count]
+                   (and (= prev-angle \L) (= pipe \7)) [nil count]
+                   (and (nil? prev-angle)
+                        (#{\F \J \L \7} pipe)) [pipe (inc count)]
+                   :else [prev-angle (inc count)])))
+             [nil 0])
+            second
+            #_(map (fn [new-y]
+                     (reduce (fn [[prev-angle count] [pipe [ny nx]]]
+                               (or (and 
+                                    (= [ny nx] [new-y x])
+                                    (horizontal-wall? pipe)
+                                    (cond
+                                      (and (= prev-angle \F) (= pipe \J)) [nil count]
+                                      (and (= prev-angle \L) (= pipe \7)) [nil count]
+                                      (and (nil? prev-angle)
+                                           (#{\F \J \L \7} pipe)) [pipe (inc count)]
+                                      :else [prev-angle (inc count)]))
+                                   [prev-angle count]))
+                             [nil 0]
+                             chain)))
+            #_(#(or (second %) 0)))
        (->> down
-            (map (fn [new-y]
-                   (first (filter (fn [[pipe [ny nx]]] (and (= pipe \-) 
-                                                            (= [ny nx] [new-y x]))) chain))))
-            (remove nil?)
-            count)
+            (reduce 
+             (fn [[prev-angle count] new-y]
+               (let [[pipe [ny nx]]
+                     (first (filter 
+                             (fn [[pipe [ny nx]]] (and (horizontal-wall? pipe) 
+                                                       (= [ny nx] [new-y x]))) chain))]
+                 (cond
+                   (and (= prev-angle \F) (= pipe \J)) [nil count]
+                   (and (= prev-angle \L) (= pipe \7)) [nil count]
+                   (and (nil? prev-angle)
+                        (#{\F \J \L \7} pipe)) [pipe (inc count)]
+                   :else [prev-angle (inc count)])))
+             [nil 0])
+            second
+            #_(#(or (second %) 0)))
        (->> left
             (map (fn [new-x]
-                   (first (filter (fn [[pipe [ny nx]]] (and (= pipe \|) 
+                   (first (filter (fn [[pipe [ny nx]]] (and (vertical-wall? pipe) 
                                                             (= [ny nx] [y new-x]))) chain))))
             (remove nil?)
             count)
        (->> right
             (map (fn [new-x]
-                   (first (filter (fn [[pipe [ny nx]]] (and (= pipe \|) 
+                   (first (filter (fn [[pipe [ny nx]]] (and (vertical-wall? pipe) 
                                                             (= [ny nx] [y new-x]))) chain))))
             (remove nil?)
-            count)])))
+            count)]
+      (->> up
+            (reduce 
+             (fn [[prev-angle count] new-y]
+               (let [[pipe [ny nx]]
+                     (first (filter 
+                             (fn [[pipe [ny nx]]] (= [ny nx] [new-y x])) chain))]
+                 #_(prn ">> prev-angle" prev-angle "pipe" pipe "count" count) 
+                 (cond
+                   (or (and (= prev-angle \F) (= pipe \J))
+                       #_(and (= prev-angle \L) (= pipe \7))
+                       #_(and (= prev-angle \J) (= pipe \F))
+                       (and (= prev-angle \7) (= pipe \L)))
+                   [nil count]
+                   (#{\F \J \L \7} pipe) [pipe (inc count)]
+                   (nil? pipe) [nil count]
+                   (= pipe \|) [prev-angle count]
+                   pipe [prev-angle (inc count)]
+                   :else [prev-angle count])))
+             [nil 0])
+            second))))
 
 (defn collect-chain-from-point [pipes sx sy]
   (loop [coords (reverse (next-coords-for-start pipes sx sy))
@@ -112,9 +169,10 @@
                  (conj chain [pipe coords])
                  (assoc-in visited coords true)))
         chain))))
-
+;; Returns [x y]
 (defn starting-point [pipes]
-  (let [[sy sx] 
+  [62 63]
+  #_(let [[sy sx] 
         (->> pipes
              (map-indexed (fn [y line]
                             (vector y (.indexOf line \S))))
@@ -123,15 +181,14 @@
     [sx sy]))
 
 (defn point-inside-of-chain? [pipes chain point]
-  (let [straight-pipes-in-chain (count-straight-pipes-in-chain pipes chain point)
-        pipes-in-chain (count-pipes-in-chain pipes chain point)
-        ]
+  (let [walls-and-angles-in-chain (count-straight-pipes-in-chain pipes chain point)]
 
-
-    (not-every? (fn [[a b]]
-                     (or (= 0 a)
-                         (= (mod b 2) 0)))
-            [pipes-in-chain straight-pipes-in-chain])))
+    (if (and (> walls-and-angles-in-chain 0)
+             (= (mod walls-and-angles-in-chain 2) 1))
+      (prn ">>>>> point" point walls-and-angles-in-chain)
+      (prn "point" point walls-and-angles-in-chain))
+    (and (> walls-and-angles-in-chain 0)
+         (= (mod walls-and-angles-in-chain 2) 1))))
 
 (defn main []
   (let [lines (read-file "input.txt")
@@ -139,11 +196,15 @@
         [sx sy] (starting-point pipes)
         chain (collect-chain-from-point pipes sx sy)
         ]
+    #_(prn "pipes")
+#_    (doall (map prn pipes))
+    #_(point-inside-of-chain? pipes chain [11 7])
+  
     (->> (mapcat (fn [y]
-                  (filter (fn [x]
-                         (point-inside-of-chain? pipes chain [y x]))
-                       (range (count (first lines)))))
-                (range (count lines)))
+                   (filter (fn [x]
+                             (point-inside-of-chain? pipes chain [y x]))
+                           (range (count (first pipes)))))
+                 (range (count pipes)))
          count)
- #_   (prn (count (filter (fn [[_ [y x]]] (= y 1)) chain)))
-#_    (point-inside-of-chain? pipes chain [1 0])))
+    #_   (prn (count (filter (fn [[_ [y x]]] (= y 1)) chain)))
+    #_    (point-inside-of-chain? pipes chain [1 0])))
